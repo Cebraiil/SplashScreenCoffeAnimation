34f89546498c10317f8aa9014cc88c0c4ff30280
Ayman Jibrael <aymanjibrael@outlook.com>
2023-01-18 13:11:00 +0300

    Initial Commit

---
+++ SplashScreenCoffe/SplashScreenCoffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenCoffeApp.swift
+//  SplashScreenCoffe
+//
+//  Created by Ayman on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenCoffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
